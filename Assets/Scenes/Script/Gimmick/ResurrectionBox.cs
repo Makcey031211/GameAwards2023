1c6e98c2f9d32d1a46660c94a4494be76760f0e7
WNHS10079\81907 <yuuki4617@icloud.com>
2023-03-26 04:02:33 +0900

    2023/03/26:復活箱を修正※詳細はプルリクのコメントに書きます。 by髙橋

--- Assets/Scenes/Script/Gimmick/ResurrectionBox.cs
+++ Assets/Scenes/Script/Gimmick/ResurrectionBox.cs
@@ -14,8 +14,20 @@ public class ResurrectionBox : MonoBehaviour
     [SerializeField, Header("生成までの待ち時間(秒)")]
     private float delayTimer = 0.1f;
 
+    [SerializeField, Header("アニメーション時間(秒)")]
+    private float animationTime = 0.1f;
+
+    [SerializeField, Header("アニメーションの遅延時間(秒)")]
+    private float animationDelayTime = 0.1f;
+
+    [SerializeField, Header("箱の消滅時間(秒)")]
+    private float boxDisTime = 0.1f;
+
     [SerializeField, Header("生成音SE")]
-    private AudioClip sound;
+    private AudioClip generatedSound;
+
+    [SerializeField, Header("消滅音SE")]
+    private AudioClip disSound;
 
     //- 花火点火スクリプト
     FireFlower FireflowerScript;
@@ -40,9 +52,9 @@ public class ResurrectionBox : MonoBehaviour
     private void Update()
     {
         if (FireflowerScript.isExploded)
-        {//爆発した後
+        {//- 爆発した後
             if (!isOnce)
-            { // 爆発直後
+            { //- 爆発直後
                 isOnce = true;
                 //- SpawnPlayerメソッドをdelayTime秒後に呼び出す
                 StartCoroutine(SpawnPlayer(delayTime));
@@ -54,13 +66,57 @@ public class ResurrectionBox : MonoBehaviour
     {
         //- delayTime秒待機する
         yield return new WaitForSeconds(delayTime);
-        //- プレイヤーを生成する
-        Instantiate(PlayerPrefab, transform.position, Quaternion.identity);
+
+        //- アニメーション用の変数
+        float elapsed = 0;
+
+        //- 徐々に生成するプレイヤーの数
+        int numPlayers = 1;
+
+        //- 生成位置(Z)
+        float playerPosZ = 1.5f;
+
+        //- プレイヤーを徐々に生成する
+        for (int i = 0; i < numPlayers; i++)
+        {
+            //- プレイヤーを生成する
+            Vector3 spawnPosition = new Vector3(
+                transform.position.x, transform.position.y, playerPosZ);
+            GameObject player = Instantiate(PlayerPrefab, spawnPosition, Quaternion.identity);
+
+            //- 音の再生
+            gameObject.GetComponent<AudioSource>().PlayOneShot(generatedSound);
+
+            //- SceneChangeスクリプトのプレイヤー生存フラグをtrueにする
+            sceneChange.bIsLife = true;
+
+            //- 徐々に生成するアニメーション
+            while (elapsed < animationTime)
+            {
+                float t = elapsed / animationTime;
+                player.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, t);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            //- アニメーションの遅延
+            yield return new WaitForSeconds(animationDelayTime);
+        }
+
+        //- プレイヤーを生成後、復活箱を徐々に消滅
+        float startTime = Time.time;
+        Vector3 initialScale = transform.localScale;
+
         //- 音の再生
-        gameObject.GetComponent<AudioSource>().PlayOneShot(sound);
-        //- SceneChangeスクリプトのプレイヤー生存フラグをtrueにする
-        sceneChange.bIsLife = true;
-        //- プレイヤーを生成後、復活箱を削除
+        gameObject.GetComponent<AudioSource>().PlayOneShot(disSound);
+
+        //- 復活箱を徐々に消滅させる
+        while (Time.time < startTime + boxDisTime)
+        {
+            float t = (Time.time - startTime) / boxDisTime;
+            transform.localScale = Vector3.Lerp(initialScale, Vector3.zero, t);
+            yield return null;
+        }
         Destroy(gameObject);
     }
 }
