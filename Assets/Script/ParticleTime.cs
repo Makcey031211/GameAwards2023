5e6b1f9778648f7779298ead9fb6e211fed700ba
WNHS10278\macke <mackey031211@icloud.com>
2023-04-17 17:02:37 +0900

    2023/04/17 プレイヤーエフェクト変更 by牧野

--- Assets/Script/ParticleTime.cs
+++ Assets/Script/ParticleTime.cs
@@ -20,6 +20,8 @@ public class ParticleTime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(particleSystem.main.duration);
+        Debug.Log(particleSystem.time);
         sceneChange.SetParticleTime(particleSystem.time, particleSystem.main.duration);
         if(particleSystem.time == particleSystem.main.duration)
         {
