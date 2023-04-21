b6b574f23c427577b0a0f9c6851fe5641b849f4a
WNHS10131\okoto <o.kotomi.1.3@gmail.com>
2023-04-21 13:49:56 +0900

    2023/04/21 タイトル画面のシーン遷移追加、仮ゲーム終了ボタンの作成 by大川

---
+++ Assets/UIData/EndGame.cs
@@ -0,0 +1,11 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("èIóπ");
+    }   
+}
