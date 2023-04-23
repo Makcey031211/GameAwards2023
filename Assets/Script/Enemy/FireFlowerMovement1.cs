ebf2b7b32934187485118df1ac26cb8019c40603
WNHS10079\81907 <yuuki4617@icloud.com>
2023-04-23 22:49:24 +0900

    2023/04/23 ステージ3の花火玉の停止挙動修正 by髙橋

--- Assets/Script/Enemy/FireFlowerMovement1.cs
+++ Assets/Script/Enemy/FireFlowerMovement1.cs
@@ -23,14 +23,11 @@ public class FireFlowerMovement1 : MonoBehaviour
     [SerializeField, Header("移動時間")]
     private float travelTime = 1.0f;
 
-    [SerializeField, Header("終了時間")]
-    private float stopTime = 2.0f;
+    [SerializeField, Header("待機時間")]
+    private float waitTime = 2.0f;
 
     //- 移動中かどうか
-    private bool isMoving  = true;
-
-    //- 停止中かどうか
-    private bool isStopped = false;
+    private bool isMoving = true;
 
     private Vector3 startPosition; // 開始地点
     private Vector3 endPosition;   // 終了地点
@@ -41,21 +38,9 @@ public class FireFlowerMovement1 : MonoBehaviour
     {
         startPosition = transform.position;
 
-        fireworks = GetComponent<FireworksModule>();
+        endPosition = GetEndPosition();
 
-        //- 移動の状態遷移
-        switch (moveDirection)
-        {
-            case MoveDirection.Horizontal:
-                endPosition = startPosition + Vector3.right * moveDistance;
-                break;
-            case MoveDirection.Vertical:
-                endPosition = startPosition + Vector3.up * moveDistance;
-                break;
-            case MoveDirection.Diagonal:
-                endPosition = startPosition + new Vector3(moveDistance, moveDistance, 0);
-                break;
-        }
+        fireworks = GetComponent<FireworksModule>();
     }
 
     private void Update()
@@ -63,53 +48,58 @@ public class FireFlowerMovement1 : MonoBehaviour
         if (!fireworks.IsExploded)
         {
             //- 停止していない時
-            if (!isStopped)
+            if (isMoving)
             {
-                //- 線形補間を使ってオブジェクトを移動
-                float t = Mathf.PingPong(Time.time / travelTime, 1.0f);
-                transform.position = Vector3.Lerp(startPosition, endPosition, t);
-
-                //- 終了地点が一定値以下なら
-                if (Vector3.Distance(transform.position, endPosition) < 0.01f)
-                {
-                    isStopped = true;
-                    StartCoroutine(StopAndWait());
-                }
-            }
-            else if (isMoving) //- 移動している時
-            {
-                StartCoroutine(WaitAndMoveBack());
+                Move();
             }
         }
     }
 
-    /// <summary>
-    /// 指定した時間の間、オブジェクトを待機させる関数
-    /// </summary>
-    /// <returns>isStopped</returns>
-    private IEnumerator StopAndWait()
+    private Vector3 GetEndPosition()
     {
-        //- stopTime分待機させる
-        yield return new WaitForSeconds(stopTime);
-
-        isStopped = false;
-        isMoving  = true;
+        switch (moveDirection)
+        {
+            case MoveDirection.Horizontal:
+                return startPosition + Vector3.right * moveDistance;
+            case MoveDirection.Vertical:
+                return startPosition + Vector3.up * moveDistance;
+            case MoveDirection.Diagonal:
+                return startPosition + new Vector3(moveDistance, moveDistance, 0);
+            default:
+                return startPosition;
+        }
     }
 
-    /// <summary>
-    /// 待機が終了したら、再び移動させる関数
-    /// </summary>
+    private void Move()
+    {
+        //- 線形補間でオブジェクトを移動させる
+        float t = Mathf.PingPong(Time.time / travelTime, 1.0f);
+        transform.position = Vector3.Lerp(startPosition, endPosition, t);
+
+        if (Vector3.Distance(transform.position, endPosition) < 0.01f)
+        {
+            if (endPosition == startPosition) // StartPositionに戻る場合
+            {
+                endPosition = GetEndPosition(); // EndPositionを更新
+                isMoving = true;
+            }
+            else
+            {
+                StartCoroutine(WaitAndMoveBack());
+                isMoving = false;
+            }
+        }
+    }
+
     private IEnumerator WaitAndMoveBack()
     {
-        isMoving = false;
-        yield return new WaitForSeconds(stopTime);
+        yield return new WaitForSeconds(waitTime);
 
-        //- 現在の終点を始点、始点を現在の終点に設定して、移動を再開する
+        //- 現在の始点を終点、終点を現在の始点に設定して、移動を再開する
         Vector3 temp  = endPosition;
         endPosition   = startPosition;
         startPosition = temp;
 
-        isStopped = false;
-        isMoving  = true;
+        isMoving = true;
     }
 }
