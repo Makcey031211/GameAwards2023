2838b5b26cbbf9811a1ca57aa570e000de6b5045
WNHS10278\macke <mackey031211@icloud.com>
2023-04-25 10:29:07 +0900

    2023/04/24 演出シーンでぬし花火が打ちあがるように追加 by牧野

--- Assets/Script/MovieManager.cs
+++ Assets/Script/MovieManager.cs
@@ -9,19 +9,26 @@ public class MovieManager : MonoBehaviour
     [SerializeField, Header("フェードの長さ(秒)")]
     private float FadeTime;
 
-    [SerializeField, Header("花火発生の遅延時間(秒)")]
-    private float DelayFireflowerTime;
+    [SerializeField, Header("フェード後打ち上げるまでの時間(秒)")]
+    private float DelayBeltTime;
 
-    [SerializeField, Header("花火の後、フェードが始まるまでの時間(秒)")]
+    [SerializeField, Header("打ち上げ後、爆発するまでの時間(秒)")]
+    private float DelayFireflowerTime;
+    
+
+    [SerializeField, Header("爆発後、フェードが始まるまでの時間(秒)")]
     private float DelayFadeTime;
 
     [SerializeField, Header("ステージ描画オブジェクト")]
     private GameObject StageDrawObj;
 
+    [SerializeField, Header("ぬし花火の動きの補完")]
+    private Ease easy;
 
     private SceneChange sceneChange; // コントローラーの振動用
     private bool bPlayMovie = false; // 演出中かどうか
     private ObjectFade fade; // フェード用のスプライト
+    private GameObject movieObj; // 演出シーン内のオブジェクト
 
     void Start()
     {
@@ -60,10 +67,19 @@ public class MovieManager : MonoBehaviour
         fade.SetFade(TweenColorFade.FadeState.Out, FadeTime);
         yield return new WaitForSeconds(FadeTime);
 
+        //- 演出シーン内オブジェクトを検索
+        InitMovieObject();
+
+        //- 一定時間後、ぬし花火を打ち上げる
+        yield return new WaitForSeconds(DelayBeltTime);
+        SetActiveMovieObject(0,true);
+        AnimeBossFireflower(0);
+
         //- 一定時間後、花火を発生させる
         yield return new WaitForSeconds(DelayFireflowerTime);
         SEManager.Instance.SetPlaySE(SEManager.SoundEffect.Explosion, 1.0f, false);
-        SetActiveFireflower(0, true);
+        SetActiveMovieObject(0, false); //- ぬし花火の退場
+        SetActiveMovieObject(1, true);  //- エフェクト登場
 
         //- 一定時間後、フェードを登場させる
         yield return new WaitForSeconds(DelayFadeTime);
@@ -85,11 +101,25 @@ public class MovieManager : MonoBehaviour
         SceneManager.LoadScene("MovieVillage", LoadSceneMode.Additive); //- 演出用シーンを追加ロード   
     }
 
-    //- 特定のオブジェクトのフラグを変更する関数
-    private void SetActiveFireflower(int childNum, bool bFlag)
+    //- 演出シーンロード後、オブジェクトを入手するための初期化関数
+    private void InitMovieObject()
     {
-        GameObject obj = GameObject.Find("MovieObject"); //- オブジェクトの検索
-        obj.transform.GetChild(childNum).gameObject.SetActive(bFlag); //- フラグ変更
+        movieObj = GameObject.Find("MovieObject"); //- オブジェクトの検索
+    }
+
+    //- ぬし花火用の演出処理
+    private void AnimeBossFireflower(int childNum)
+    {
+        //- ぬし花火の置き換え
+        GameObject obj = movieObj.transform.GetChild(childNum).gameObject;
+        //- ぬし花火のスケールフェード
+        obj.transform.DOScale(new Vector3(1, 1, 1), DelayFireflowerTime).SetEase(easy);
+     }
+
+    //- 特定のオブジェクトのフラグを変更する関数
+    private void SetActiveMovieObject(int childNum, bool bFlag)
+    {
+        movieObj.transform.GetChild(childNum).gameObject.SetActive(bFlag); //- フラグ変更
     }
 
     //- 演出用シーンのアンロードを行う関数
