a9b17bf94557fb842fb157485b2162ce62530f38
WNHS10131\okoto <o.kotomi.1.3@gmail.com>
2023-05-08 16:30:59 +0900

    2023/05/08 舞台選択決定時のアニメーション追加(仮) by大川

--- Assets/UIData/2_InSelect/SelectMovePlayer.cs
+++ Assets/UIData/2_InSelect/SelectMovePlayer.cs
@@ -8,10 +8,20 @@ public class SelectMovePlayer : MonoBehaviour,ISelectHandler
     private float MoveTIme = 1.0f;
     [SerializeField, Header("ÉvÉåÉCÉÑÅ[")]
     private GameObject player;
-    
+    private Vector3 StagePos;
     public void OnSelect(BaseEventData eventData)
     {
         Vector3 pos = eventData.selectedObject.transform.position;
+        StagePos = pos;
         player.transform.DOMove(pos, MoveTIme);
+    }
+
+    public void InStageMove()
+    {
+        DOTween.Sequence()
+            .Append(player.transform.DOMoveY(5.0f, 0.2f).SetRelative(true))
+            .Join(player.transform.DOScale(new Vector3(2.0f,2.0f), 0.2f))
+            .Append(player.transform.DOMoveY(StagePos.y, 0.2f))
+            .Join(player.transform.DOScale(new Vector3(0.0f, 0.0f), 0.2f));
     }
 }
