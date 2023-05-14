0ddab939ae68c237e0ca8cc159dbeb3cc22d8dc6
WNHS10278\macke <mackey031211@icloud.com>
2023-05-15 05:53:53 +0900

    2023/05/15 トンボ花火が侵入不可テープに当たった時の処理を追加 by牧野

--- Assets/Script/DragonflyWallMove.cs
+++ Assets/Script/DragonflyWallMove.cs
@@ -34,30 +34,61 @@ public class DragonflyWallMove : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        //- 当たったオブジェクトのタグが"Stage"でなければリターン
-        if (other.gameObject.tag != "Stage") return;
         //- 待機中ならリターン
         if (IsWait) return;
-
+        //- 当たったオブジェクトのタグによって呼び出す関数を変える
+        if (other.gameObject.tag == "Stage")               HitStage();
+        if (other.gameObject.tag == "PlayerBlock") HitPlayerBlock();
+    }
+    void HitStage()
+    {      
         //- 自身の方向属性によって移動方向を反転
-        switch(dirType)
+        switch (dirType)
         {
             case DirectionType.Up:
                 module.movedir.y *= -1;
-                IsWait = true;
                 break;
             case DirectionType.Under:
                 module.movedir.y *= -1;
-                IsWait = true;
                 break;
             case DirectionType.Left:
                 module.movedir.x *= -1;
-                IsWait = true;
                 break;
             case DirectionType.Rgiht:
                 module.movedir.x *= -1;
-                IsWait = true;
                 break;
         }
+        //- 待機フラグをオン
+        IsWait = true;
+    }
+    void HitPlayerBlock()
+    {
+        Debug.Log("ほへ");
+        //- 自身の方向属性によって移動方向を反転
+        switch (dirType)
+        {
+            case DirectionType.Up:
+                module.movedir.y = 0;
+                if (module.movedir.x >= 0) module.movedir.x =  1;
+                if (module.movedir.x <= 0) module.movedir.x = -1;
+                break;
+            case DirectionType.Under:
+                module.movedir.y = 0;
+                if (module.movedir.x >= 0) module.movedir.x = 1;
+                if (module.movedir.x <= 0) module.movedir.x = -1;
+                break;
+            case DirectionType.Left:
+                if (module.movedir.y >= 0) module.movedir.y =  1;
+                if (module.movedir.y <= 0) module.movedir.y = -1;
+                module.movedir.x = 0;
+                break;
+            case DirectionType.Rgiht:
+                if (module.movedir.y >= 0) module.movedir.y = 1;
+                if (module.movedir.y <= 0) module.movedir.y = -1;
+                module.movedir.x = 0;
+                break;
+        }
+        //- 待機フラグをオン
+        IsWait = true;
     }
 }
