540386c1b38fd9c656a0bdd9f161ca0ec5fd291e
InoueKazumasa <karura.a3003@gmail.com>
2023-05-24 20:47:38 +0900

    2023/05/24 ステージセレクト画面で進行度が一番高いステージをデフォルト選択 by井上

--- Assets/Script/DefaultSelectStage.cs
+++ Assets/Script/DefaultSelectStage.cs
@@ -6,24 +6,48 @@ using UnityEngine.UI;
 
 public class DefaultSelectStage : MonoBehaviour
 {
+    private enum SelectType
+    {
+        Sato,
+        Stage
+    }
+
+    [SerializeField]
+    private SelectType type;
     [SerializeField]
     private Button[] stageButton;
     [SerializeField]
     private SaveManager saveManager;
+    [SerializeField, Header("type‚ªStage‚ÌŽž‚Ì‚Ý—˜—p")]
+    private int stageNum;
+    [SerializeField]
+    private GameObject player;
 
     int maxClearVillege = 0;
 
     // Start is called before the first frame update
     void Awake()
     {
-        for(int i = 1; i <= 3 ; i++) {
-            if (saveManager.GetStageClear(i * 10)) maxClearVillege++;
-            Debug.Log(maxClearVillege);
+        int satoValue = 1;
+        int stageValue = 0;
+        if (type == SelectType.Sato) satoValue *= 10;
+        if (type == SelectType.Stage) stageValue = (stageNum - 1) * 10;
+
+        for(int i = 0; i < stageButton.Length - 1; i++) {
+            if (saveManager.GetStageClear((i + 1) * satoValue + stageValue)) maxClearVillege++;
         }
 
-        stageButton[maxClearVillege].gameObject.SetActive(true);
+        if (type == SelectType.Stage && saveManager.GetStageClear(10 * stageNum)) {
+            maxClearVillege = 0;
+        }
+
+            stageButton[maxClearVillege].gameObject.SetActive(true);
         stageButton[maxClearVillege].enabled = true;
         stageButton[maxClearVillege].Select();
+
+        if(type == SelectType.Stage) {
+            player.transform.position = stageButton[maxClearVillege].transform.position;
+        }
 
         Debug.Log(EventSystem.current.currentSelectedGameObject.name);
     }
