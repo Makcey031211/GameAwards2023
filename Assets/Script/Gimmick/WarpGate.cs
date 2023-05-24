fabab62373110ea9e6b602982012e593e1eee28b
WNHS10452\rrine <nhs10452@nhs.hal.ac.jp>
2023-05-24 17:49:42 +0900

    2023/05/24 ワープホールをトンボ花火に適応by中村

--- Assets/Script/Gimmick/WarpGate.cs
+++ Assets/Script/Gimmick/WarpGate.cs
@@ -40,6 +40,30 @@ public class WarpGate : MonoBehaviour
                 cc.enabled = true;
             }
         }
+
+        //- ワープホールに接触したら指定した位置にワープする
+        if (other.gameObject.name == "DragonflyModule")
+        {
+            //- プレイヤーとワープゲートの距離を計算
+            Vector3 DFPos = other.transform.position;
+            Vector3 gatePos = transform.position;
+            float distance = Vector3.Distance(DFPos, gatePos);
+            //ワープから出てくる方向と位置調整のために入射角を取得
+            Vector3 warpdistance = WarpHole.transform.position - other.transform.position;
+
+            //- プレイヤーをワープする処理
+            if (distance < radius)
+            {
+                //var cc = other.gameObject.GetComponent<CharacterController>();
+                //cc.enabled = false;
+                //- プレイヤーを指定した位置にワープさせる
+                other.transform.position = warpPoint.position + warpdistance * 1.2f;
+                Vector3 pos = other.transform.position;
+                pos.z = 0.0f;
+                other.transform.position = pos;
+                //cc.enabled = true;
+            }
+        }
     }
 
     //private void OnTriggerStay(Collider other)
