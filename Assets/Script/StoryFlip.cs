2f103caee485d26d08f10a3ba8900bcd749f8012
WNHS10079\81907 <yuuki4617@icloud.com>
2023-05-27 23:39:07 +0900

    2023/05/27 連続押し防止、コントローラー処理追加 by髙橋

--- Assets/Script/StoryFlip.cs
+++ Assets/Script/StoryFlip.cs
@@ -17,17 +17,8 @@ using UnityEngine.UI;
 
 public class StoryFlip : MonoBehaviour
 {
-    //- シーン遷移先を設定する
-    enum E_SceneTransDes
-    {
-        Village,
-        GameEnd
-    }
-
     [SerializeField]
     private GameObject[] flips;     // ストーリーのイラスト
-    [SerializeField, Header("シーン遷移先")]
-    private E_SceneTransDes scene = E_SceneTransDes.Village;
     [SerializeField, Header("フェード秒数")]
     private float FadeTime;
 
@@ -91,16 +82,7 @@ public class StoryFlip : MonoBehaviour
         { // 最後のイラストに移り変わった時
             //SEManager.Instance.SetPlaySE(SEManager.E_SoundEffect.Click);
             DOVirtual.DelayedCall(FadeTime, () => bgmManager.DestroyBGMManager());
-            //- 紙芝居が終わった後のシーン遷移先の分岐
-            switch (scene)
-            {
-                case E_SceneTransDes.Village:
-                    fade.DOFade(1.0f, 1.5f).OnComplete(() => { SceneManager.LoadScene("1_Village"); });
-                    break;
-                case E_SceneTransDes.GameEnd:
-                    fade.DOFade(1.0f, 1.5f).OnComplete(() => { SceneManager.LoadScene("3_GameEnd"); });
-                    break;
-            }
+            fade.DOFade(1.0f, 1.5f).OnComplete(() => { SceneManager.LoadScene("1_Village"); });
         }
     }
 
@@ -139,6 +121,21 @@ public class StoryFlip : MonoBehaviour
                 trans.DOMoveX(trans.position.x + 15.0f, 1.5f).SetEase(Ease.InOutCubic).OnComplete(() => { isFlip = false; });
             }
             NowFlipNum--;
+        }
+    }
+
+    // ===================================================
+    // BackSpaceキー押下時処理
+    //
+    // 次のシーンへ移行する
+    // ===================================================
+    public void OnClose(InputAction.CallbackContext context)
+    {
+        if (NowFlipNum >= flips.Length - 1 && !isFlip)
+        { // 最後のイラストに移り変わった時
+            //SEManager.Instance.SetPlaySE(SEManager.E_SoundEffect.Click);
+            DOVirtual.DelayedCall(FadeTime, () => bgmManager.DestroyBGMManager());
+            fade.DOFade(1.0f, 1.5f).OnComplete(() => { SceneManager.LoadScene("3_GameEnd"); });
         }
     }
 }
