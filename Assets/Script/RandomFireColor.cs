bfb555da7746d1b75b2fe9884ee1aadd5d91f9ce
InoueKazumasa <karura.a3003@gmail.com>
2023-06-25 03:54:28 +0900

    2023/06/25 柳の目消えないやつ修正、RandomFireColor.csのコメント追加

--- Assets/Script/RandomFireColor.cs
+++ Assets/Script/RandomFireColor.cs
@@ -3,40 +3,42 @@ using UnityEngine;
 
 public class RandomFireColor : MonoBehaviour
 {
+    // 変化させる色
     [ColorUsage(false, true), SerializeField]
     private Color[] particleColor;
     [ColorUsage(true, true), SerializeField]
     private Color[] trailColor;
+
+    // 色を変えるパーティクルシステム
     [SerializeField]
     private ParticleSystem[] particles;
     [SerializeField]
     private ParticleSystem[] trails;
+
+    // それぞれのシェーダー内に宣言してある色のプロパティ名
     [SerializeField]
     private string particleShaderProperty;
     [SerializeField]
     private string trailShaderProperty;
 
-    private int colorNum;
-    MaterialPropertyBlock materialPropertyBlock;
+    private int colorNum; // 色のランダム指定用
+    MaterialPropertyBlock materialPropertyBlock; // 個別に色変更するために使用
 
     // Start is called before the first frame update
     void Start()
     {
         materialPropertyBlock = new MaterialPropertyBlock();
-
         colorNum = UnityEngine.Random.Range(0, particleColor.Length);
 
-        Debug.Log(colorNum);
-
         for (int i = 0; i < particles.Length; i++) {
-            int color = Shader.PropertyToID(particleShaderProperty);
-            materialPropertyBlock.SetColor(color, particleColor[colorNum]);
+            int colorProperty = Shader.PropertyToID(particleShaderProperty);
+            materialPropertyBlock.SetColor(colorProperty, particleColor[colorNum]);
             particles[i].GetComponent<ParticleSystemRenderer>().SetPropertyBlock(materialPropertyBlock);
         }
 
         for (int i = 0; i < trails.Length; i++) {
-            int color = Shader.PropertyToID(trailShaderProperty);
-            materialPropertyBlock.SetColor(color, trailColor[colorNum]);
+            int colorProperty = Shader.PropertyToID(trailShaderProperty);
+            materialPropertyBlock.SetColor(colorProperty, trailColor[colorNum]);
             trails[i].GetComponent<ParticleSystemRenderer>().SetPropertyBlock(materialPropertyBlock);
         }
     }
